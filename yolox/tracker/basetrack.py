04d3fec25581789d82a0279740fd925ca275622e
Carol102 <tranthuthuy1002@gmail.com>
2022-06-26 13:37:24 +0700

    init

---
+++ yolox/tracker/basetrack.py
@@ -0,0 +1,57 @@
+import numpy as np
+from collections import OrderedDict
+
+
+class TrackState(object):
+    New = 0
+    Tracked = 1
+    Lost = 2
+    Removed = 3
+
+
+class BaseTrack(object):
+    _count = 0
+
+    def __init__(self):
+
+        self.track_id = 0
+        self.is_activated = False
+        self.state = TrackState.New
+
+        self.history = OrderedDict()
+        self.features = []
+        self.curr_feature = None
+        self.score = 0
+        self.start_frame = 0
+        self.frame_id = 0
+        self.time_since_update = 0
+
+        # multi-camera
+        self.location = (np.inf, np.inf)
+
+    @property
+    def end_frame(self):
+        return self.frame_id
+
+    @staticmethod
+    def next_id():
+        if BaseTrack._count is None:
+            BaseTrack._count = 0
+        BaseTrack._count += 1
+        print("person", BaseTrack._count)
+        return BaseTrack._count
+
+    def activate(self, *args):
+        raise NotImplementedError
+
+    def predict(self):
+        raise NotImplementedError
+
+    def update(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def mark_lost(self):
+        self.state = TrackState.Lost
+
+    def mark_removed(self):
+        self.state = TrackState.Removed
